7ad8613f6c33353f531281137feb2699a73dcf6b
Alfredo Ramos <alfredo.ramos@yandex.com>
2017-05-13 18:09:39 -0500

    Add Spanish translations

---
+++ language/es_x_tu/imgur.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Imgur Extension for phpBB.
+ * @author Alfredo Ramos <alfredo.ramos@yandex.com>
+ * @copyright 2017 Alfredo Ramos
+ * @license GNU GPL-2.0
+ */
+
+/**
+ * @ignore
+ */
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+/**
+ * @ignore
+ */
+if (empty($lang) || !is_array($lang))
+{
+	$lang = [];
+}
+
+$lang = array_merge($lang, [
+	// ACP
+	'ACP_IMGUR'	=> 'Ajustes de Imgur',
+	'ACP_IMGUR_CLIENT_ID' => 'Client ID',
+	'ACP_IMGUR_CLIENT_SECRET' => 'Client Secret',
+	'ACP_IMGUR_ALBUM'	=> 'Álbum',
+	'ACP_IMGUR_ALBUM_EXPLAIN' => 'Identificador del álbum en donde las imágenes serán subidas. Déjalo vacío si deseas que las imágenes sean subidas en la ubicación por defecto.',
+	'ACP_IMGUR_AUTH_EXPLAIN' => 'Haz clic en el siguiente enlace para autorizar la aplicación.',
+	'ACP_IMGUR_PIN' => 'PIN',
+	'ACP_IMGUR_PIN_EXPLAIN' => 'PIN de autorización necesario para acceder a tu cuenta de Imgur.',
+	'ACP_IMGUR_SETTINGS_SAVED' => 'Los ajustes de Imgur han sido guardados satisfactoriamente.',
+
+	// Exceptions
+	'EXCEPTION_IMGUR_NO_API_DATA' => 'Client ID y Client Secret son obligatorios.',
+]);
