6f366d72e81d28314bcf2f61d18a13936da54b44
Basil Taha Alhitary <alhitary@users.noreply.github.com>
2017-10-23 00:00:42 +0300

    Update Arabic translation
    Closes #3

--- language/ar/acp/info_acp_imgur.php
+++ language/ar/acp/info_acp_imgur.php
@@ -32,5 +32,7 @@ $lang = array_merge($lang, [
 	'ACP_IMGUR_AUTH_EXPLAIN' => 'أنقر على الرابط التالي للمُصادقة على هذه الخدمة.',
 	'ACP_IMGUR_PIN' => 'رمز التعريف الشخصي ',
 	'ACP_IMGUR_PIN_EXPLAIN' => 'اكتب رقم الـ PIN للمُصادقة والوصول إلى حسابك في Imgur.',
-	'ACP_IMGUR_SETTINGS_SAVED' => 'تم حفظ الإعدادات بنجاح.'
+	'ACP_IMGUR_SETTINGS_SAVED' => 'تم حفظ الإعدادات بنجاح.',
+
+	'LOG_IMGUR_DATA' => '<strong>تم تغيير بيانات الإضافة : Imgur</strong>'
 ]);
