f40c90e1246f7d8ceb7d7b5612f0369376ad9578
Alfredo Ramos <alfredo.ramos@yandex.com>
2017-11-08 11:49:18 -0600

    Insert BBCode at cursor position

--- styles/all/theme/js/imgur.js
+++ styles/all/theme/js/imgur.js
@@ -8,6 +8,43 @@
 (function($) {
 	'use strict';
 
+	// Insert text at cursor position
+	// Slightly modified version
+	// https://gist.github.com/mathiasbynens/326491
+	$.fn.insertAtCaret = function($text) {
+		return this.each(function() {
+			if (document.selection) {
+				// Internet Explorer
+				this.focus();
+				var $selection = document.selection.createRange();
+				$selection.text = $text;
+				this.focus();
+			} else if (this.selectionStart || this.selectionStart == '0') {
+				// Modern browsers
+				var $position = {
+					start: this.selectionStart,
+					end: this.selectionEnd,
+					top: this.scrollTop
+				};
+
+				// Insert text
+				this.value = this.value.substring(0, $position.start)
+					+ $text
+					+ this.value.substring($position.end, this.value.length);
+				this.focus();
+
+				// Update position
+				this.selectionStart = $position.start + $text.length;
+				this.selectionEnd = $position.start + $text.length;
+				this.scrollTop = $position.top;
+			} else {
+				// Fallback
+				this.value += $text;
+				this.focus();
+			}
+		});
+	};
+
 	// Show image selection window
 	$(document).on('click', '.imgur-button', function() {
 		$('#imgur-image').trigger('click');
@@ -60,7 +97,7 @@
 				for (var $k in $contentBody) {
 					if ($contentBody.hasOwnProperty($k)) {
 						if ($contentBody[$k].length > 0) {
-							$contentBody[$k].val($contentBody[$k].val() + $bbcode);
+							$contentBody[$k].insertAtCaret($bbcode);
 						}
 					}
 				}
