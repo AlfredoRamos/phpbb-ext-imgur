43830ba8084ab590fd5c564a3ccb5708682d21c3
Alfredo Ramos <alfredo.ramos@yandex.com>
2017-11-21 14:35:15 -0600

    Travis CI improvements

--- travis/setup-extension-dependencies.sh
+++ travis/setup-extension-dependencies.sh
@@ -4,7 +4,12 @@ set -e
 set -x
 
 EXTNAME="${1}"
+EXTDEPS="${2}"
 
-cd phpBB/ext/"${EXTNAME}"
-composer install --prefer-dist --no-dev --no-interaction
-cd ../../../../
+# Check if package have dependencies in the
+# 'require' object, inside the composer.json file
+if [[ "${EXTDEPS}" == '1' ]]; then
+	cd phpBB/ext/"${EXTNAME}"
+	composer install --prefer-dist --no-dev --no-interaction
+	cd ../../../../
+fi
