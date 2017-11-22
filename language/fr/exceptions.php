d4558b1606d7b1b6cafece12b0c85c5cff546cbf
Alfredo Ramos <alfredo.ramos@yandex.com>
2017-11-22 11:42:40 -0600

    Add French translation by @panteror

---
+++ language/fr/exceptions.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Imgur Extension for phpBB.
+ * @author Alfredo Ramos <alfredo.ramos@yandex.com>
+ * @copyright 2017 Alfredo Ramos
+ * @license GPL-2.0
+ */
+
+/**
+ * @ignore
+ */
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+/**
+ * @ignore
+ */
+if (empty($lang) || !is_array($lang))
+{
+	$lang = [];
+}
+
+$lang = array_merge($lang, [
+	'EXCEPTION_IMGUR_NO_API_DATA' => 'Client ID et Client Secret sont obligatoires.',
+	'EXCEPTION_IMGUR_AJAX_ONLY' => 'Cette route ne peut être utilisée que sur les appels AJAX.',
+	'EXCEPTION_IMGUR_BAD_REQUEST' => '%s'
+]);
