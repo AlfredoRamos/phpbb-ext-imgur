aa32ef3046e9e9530061fbff2a3b35e4ee750d38
Alfredo Ramos <alfredo.ramos.sanchez@gmail.com>
2017-11-24 18:27:05 -0600

    New translations imgur.php (Spanish, Mexico)

---
+++ es-MX/language/es_x_tu/imgur.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Imgur Extension for phpBB.
+ * @author Alfredo Ramos <alfredo.ramos@yandex.com>
+ * @copyright 2017 Alfredo Ramos
+ * @license GPL-2.0
+ */
+
+/**
+ * @ignore
+ */
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+/**
+ * @ignore
+ */
+if (empty($lang) || !is_array($lang))
+{
+	$lang = [];
+}
+
+$lang = array_merge($lang, [
+	'IMGUR_BUTTON_EXPLAIN' => 'Upload the selected images to Imgur and post its content'
+]);
