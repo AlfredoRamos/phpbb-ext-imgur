c2b188dc6298d8d55d72899033a0b2961d8d3133
Alfredo Ramos <alfredo.ramos.sanchez@gmail.com>
2019-05-10 16:43:56 -0500

    Add posting box panel functional tests

--- tests/functional/imgur_test.php
+++ tests/functional/imgur_test.php
@@ -42,36 +42,6 @@ class imgur_test extends phpbb_functional_test_case
 		$this->login();
 	}
 
-	public function test_imgur_input()
-	{
-		$crawler = self::request('GET', sprintf(
-			'posting.php?mode=post&f=2&sid=%s',
-			$this->sid
-		));
-
-		$elements = [
-			'button' => $crawler->filter('#postingbox #format-buttons .imgur-button'),
-			'input' => $crawler->filter('#postingbox #imgur-image'),
-			'select' => $crawler->filter('#postingbox #format-buttons .imgur-output-select')
-		];
-
-		$this->assertSame(1, $elements['button']->count());
-		$this->assertSame(
-			$this->lang('IMGUR_BUTTON_EXPLAIN'),
-			$elements['button']->attr('title')
-		);
-
-		$this->assertSame(1, $elements['input']->count());
-		$this->assertSame(1, preg_match(
-			'#^/app\.php/imgur/upload/\w+$#',
-			$elements['input']->attr('data-ajax-action')
-		));
-		$this->assertSame('image', $elements['input']->attr('data-output-type'));
-		$this->assertSame('t', $elements['input']->attr('data-thumbnail-size'));
-
-		$this->assertSame(1, $elements['select']->count());
-	}
-
 	public function test_acp_form_settings()
 	{
 		$this->admin_login();
@@ -113,5 +83,52 @@ class imgur_test extends phpbb_functional_test_case
 
 		$this->assertTrue($form->has('imgur_thumbnail_size'));
 		$this->assertSame('t', $form->get('imgur_thumbnail_size')->getValue());
+	}
+
+	public function test_imgur_input()
+	{
+		$crawler = self::request('GET', sprintf(
+			'posting.php?mode=post&f=2&sid=%s',
+			$this->sid
+		));
+
+		$elements = [
+			'button' => $crawler->filter('#postingbox #format-buttons .imgur-button'),
+			'input' => $crawler->filter('#postingbox #imgur-image'),
+			'select' => $crawler->filter('#postingbox #format-buttons .imgur-output-select'),
+			'tab' => $crawler->filter('#postform #imgur-panel-tab > a'),
+			'upload' => $crawler->filter('#postform #imgur-panel .imgur-upload-button'),
+			'fields' => $crawler->filter('#postform #imgur-panel .imgur-output-fields dl')
+		];
+
+		$this->assertSame(1, $elements['button']->count());
+		$this->assertContains(
+			$this->lang('IMGUR_BUTTON_EXPLAIN'),
+			$elements['button']->attr('title')
+		);
+
+		$this->assertSame(1, $elements['input']->count());
+		$this->assertSame(1, preg_match(
+			'#^/app\.php/imgur/upload/\w+$#',
+			$elements['input']->attr('data-ajax-action')
+		));
+		$this->assertSame('image', $elements['input']->attr('data-output-type'));
+		$this->assertSame('t', $elements['input']->attr('data-thumbnail-size'));
+
+		$this->assertSame(1, $elements['select']->count());
+
+		$this->assertSame(1, $elements['tab']->count());
+		$this->assertContains(
+			$this->lang('IMGUR_TAB'),
+			$elements['tab']->text()
+		);
+
+		$this->assertSame(1, $elements['upload']->count());
+		$this->assertContains(
+			$this->lang('IMGUR_UPLOAD'),
+			$elements['upload']->text()
+		);
+
+		$this->assertSame(4, $elements['fields']->count());
 	}
 }
