44289e6d0ea949681c9c59ee024246689bcfca1d
Alfredo Ramos <alfredo.ramos.sanchez@gmail.com>
2020-02-09 18:00:12 -0600

    Create dedicated file for polyfills

--- styles/all/theme/js/functions.js
+++ styles/all/theme/js/functions.js
@@ -7,60 +7,6 @@
 
 'use strict';
 
-// Polyfill for NodeList.forEach()
-// https://developer.mozilla.org/en-US/docs/Web/API/NodeList/forEach#Polyfill
-if (!NodeList.prototype.forEach) {
-	NodeList.prototype.forEach = Array.prototype.forEach;
-}
-
-// Polifyll for Array.isArray()
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/isArray#Polyfill
-if (!Array.isArray) {
-	Array.isArray = function(arg) {
-		return Object.prototype.toString.call(arg) === '[object Array]';
-	};
-}
-
-// Polyfill for Object.entries()
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries#Polyfill
-if (!Object.entries) {
-	Object.entries = function( obj ){
-		let ownProps = Object.keys(obj);
-		let i = ownProps.length;
-		let resArray = new Array(i);
-
-		while (i--) {
-			resArray[i] = [ownProps[i], obj[ownProps[i]]];
-		}
-
-		return resArray;
-	};
-}
-
-// Polyfill for Element.matches()
-// https://developer.mozilla.org/en-US/docs/Web/API/Element/matches#Polyfill
-if (!Element.prototype.matches) {
-	Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
-}
-
-// Polyfill for Element.closest()
-// https://developer.mozilla.org/en-US/docs/Web/API/Element/closest#Polyfill
-if (!Element.prototype.closest) {
-	Element.prototype.closest = function(s) {
-		let el = this;
-
-		do {
-			if (el.matches(s)) {
-				return el;
-			}
-
-			el = el.parentElement || el.parentNode;
-		} while (el !== null && el.nodeType === 1);
-
-		return null;
-	};
-}
-
 /**
  * Show errors in a modal window.
  *
