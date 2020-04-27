bc1d584929e7ffc4fc686bc4d4d7c046274a2698
Alfredo Ramos <alfredo.ramos.sanchez@gmail.com>
2020-04-26 20:27:18 -0500

    New translations controller.php (French)

---
+++ language/fr/controller.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Imgur extension for phpBB.
+ * @author Alfredo Ramos <alfredo.ramos@yandex.com>
+ * @copyright 2017 Alfredo Ramos
+ * @license GPL-2.0-only
+ */
+
+/**
+ * @ignore
+ */
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+/**
+ * @ignore
+ */
+if (empty($lang) || !is_array($lang))
+{
+	$lang = [];
+}
+
+$lang = array_merge($lang, [
+	'EXCEPTION_IMGUR_NO_API_DATA' => 'Client ID and Client Secret are mandatory.',
+	'EXCEPTION_IMGUR_AJAX_ONLY' => 'This route can only be used on AJAX calls.',
+
+	'UPLOAD_ERR_INI_SIZE' => 'The uploaded file exceeds the <samp>upload_max_filesize</samp> directive in <samp>php.ini</samp>',
+	'UPLOAD_ERR_FORM_SIZE' => 'The uploaded file exceeds the <samp>MAX_FILE_SIZE</samp> directive that was specified in the HTML form',
+	'UPLOAD_ERR_PARTIAL' => 'The uploaded file was only partially uploaded',
+	'UPLOAD_ERR_NO_FILE' => 'No file was uploaded',
+	'UPLOAD_ERR_NO_TMP_DIR' => 'Missing a temporary folder',
+	'UPLOAD_ERR_CANT_WRITE' => 'Failed to write file to disk',
+	'UPLOAD_ERR_EXTENSION' => 'A PHP extension stopped the file upload',
+
+	'IMGUR_AUTHORIZATION' => 'Authorization',
+	'IMGUR_AUTHORIZATION_EXPLAIN' => 'Imgur authorization in progress, please wait.',
+	'IMGUR_AUTHORIZED' => 'You already authorized the Imgur application, you can close this window.'
+]);
