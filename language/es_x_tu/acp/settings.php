f6869723a15ce3438ec89942d58bde1712e59c53
Alfredo Ramos <alfredo.ramos.sanchez@gmail.com>
2020-04-26 19:54:33 -0500

    New translations settings.php (Spanish, Mexico)

--- language/es_x_tu/acp/settings.php
+++ language/es_x_tu/acp/settings.php
@@ -25,26 +25,26 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, [
 	'ACP_IMGUR_REGISTER' => 'Registrar una nueva aplicación',
-	'ACP_IMGUR_REGISTER_EXPLAIN' => 'Si no cuentas con las credenciales de la API que se piden a continuación, inicia sesión en tu cuenta de Imgur y registra una nueva aplicación con el siguiente enlace.<br><strong>Si ya cuentas con las credenciales de la API, no necesitas crear una nueva aplicación.</strong>',
+	'ACP_IMGUR_REGISTER_EXPLAIN' => 'Si no cuenta con las credenciales de la API que se piden a continuación, inicie sesión en su cuenta de Imgur y registre una nueva aplicación con el siguiente enlace.<br><strong>Si ya cuenta con las credenciales de la API, no necesita crear una nueva aplicación.</strong>',
 	'ACP_IMGUR_AUTHORIZE' => 'Autorizar acceso',
-	'ACP_IMGUR_AUTHORIZE_EXPLAIN' => 'Necesitas autorizar la aplicación para poder subir las imágenes a tu cuenta.',
+	'ACP_IMGUR_AUTHORIZE_EXPLAIN' => 'Necesita autorizar la aplicación para poder subir las imágenes a su cuenta.',
 
 	'ACP_IMGUR_API_SETTINGS' => 'Ajustes de la API',
-	'ACP_IMGUR_API_SETTINGS_EXPLAIN' => '<p>Aquí puedes añadir los datos necesarios para la API de Imgur. Consulta las <a href="https://www.phpbb.com/customise/db/extension/imgur/faq" rel="external nofollow noreferrer noopener" target="_blank"><strong>Preguntas Frecuentes</strong></a> para obtener más información. Si requieres de ayuda, por favor visita la sección de <a href="https://www.phpbb.com/customise/db/extension/imgur/support" rel="external nofollow noreferrer noopener" target="_blank"><strong>Soporte</strong></a>.</p>',
+	'ACP_IMGUR_API_SETTINGS_EXPLAIN' => '<p>Aquí puede añadir los datos necesarios para la API de Imgur. Consulte las <a href="https://www.phpbb.com/customise/db/extension/imgur/faq" rel="external nofollow noreferrer noopener" target="_blank"><strong>Preguntas Frecuentes</strong></a> para obtener más información. Si requiere de ayuda, por favor visite la sección de <a href="https://www.phpbb.com/customise/db/extension/imgur/support" rel="external nofollow noreferrer noopener" target="_blank"><strong>Soporte</strong></a>.</p>',
 
 	'ACP_IMGUR_CLIENT_ID' => '<em>Client ID</em>',
 	'ACP_IMGUR_CLIENT_ID_EXPLAIN' => 'Cadena de texto compuesta por números hexadecimales con una longitud de 15 caracteres.',
 	'ACP_IMGUR_CLIENT_SECRET' => '<em>Client Secret</em>',
 	'ACP_IMGUR_CLIENT_SECRET_EXPLAIN' => 'Cadena de texto compuesta por números hexadecimales con una longitud de 40 caracteres.',
 	'ACP_IMGUR_ALBUM' => 'Álbum',
-	'ACP_IMGUR_ALBUM_EXPLAIN' => 'Cadena de texto alfanumérico con una longitud igual o mayor a 5 caracteres. Será usado para almacenar las imágenes subidas. Déjalo vacío si deseas que las imágenes sean subidas en la ubicación por defecto.',
+	'ACP_IMGUR_ALBUM_EXPLAIN' => 'Cadena de texto alfanumérico con una longitud igual o mayor a 5 caracteres. Será usado para almacenar las imágenes subidas. Déjelo vacío si desea que las imágenes sean subidas en la ubicación por defecto.',
 	'ACP_IMGUR_ALBUM_DOWNLOAD' => 'Descargar copia de seguridad del álbum',
 
 	'OUTPUT_SETTINGS' => 'Ajustes de salida',
-	'OUTPUT_SETTINGS_EXPLAIN' => '<p>Aquí puedes habilitar, deshabilitar y establecer por defecto algunas opciones que cambiarán la salida de las imágenes subidas.</p>',
+	'OUTPUT_SETTINGS_EXPLAIN' => '<p>Aquí puede habilitar, deshabilitar y establecer por defecto algunas opciones que cambiarán la salida de las imágenes subidas.</p>',
 
 	'ACP_IMGUR_OUTPUT_TYPE' => 'Tipo de salida',
-	'ACP_IMGUR_OUTPUT_TYPE_EXPLAIN' => 'Debes habilitar al menos una opción. La opción por defecto debe estar habilitada, sino se utilizará la primer opción disponible.',
+	'ACP_IMGUR_OUTPUT_TYPE_EXPLAIN' => 'Debe habilitar al menos una opción. La opción por defecto debe estar habilitada, sino se utilizará la primer opción disponible.',
 
 	'ACP_IMGUR_THUMBNAIL_KEEP_PROPORTIONS' => 'Tamaños de miniatura que mantienen las proporciones de la imagen',
 	'ACP_IMGUR_THUMBNAIL_NOT_KEEP_PROPORTIONS' => 'Tamaños de miniatura que <u>no</u> mantienen las proporciones de la imagen',
