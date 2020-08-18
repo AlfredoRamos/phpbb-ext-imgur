48d43dbc443c756edbb876145a888acbfde128d8
Alfredo Ramos <alfredo.ramos.sanchez@gmail.com>
2020-08-18 01:27:29 -0500

    Add Polish translations

---
+++ language/pl/acp/info_acp_common.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Imgur extension for phpBB.
+ * @author Alfredo Ramos <alfredo.ramos@yandex.com>
+ * @copyright 2017 Alfredo Ramos
+ * @license GPL-2.0-only
+ */
+
+/**
+ * @ignore
+ */
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+/**
+ * @ignore
+ */
+if (empty($lang) || !is_array($lang))
+{
+	$lang = [];
+}
+
+$lang = array_merge($lang, [
+	'ACP_IMGUR' => 'Imgur',
+	'OUTPUT' => 'Wynik',
+	'LOG_IMGUR_DATA' => '<strong>Zmieniono dane Imgur</strong><br>» %s'
+]);
