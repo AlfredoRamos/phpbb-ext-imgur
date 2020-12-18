68387f2077487578e686a89e6aec81ea216b8630
Alfredo Ramos <alfredo.ramos.sanchez@gmail.com>
2020-12-18 13:09:49 -0600

    Fix test for Composer 2

--- travis/setup-extension-dependencies.sh
+++ travis/setup-extension-dependencies.sh
@@ -9,6 +9,14 @@ EXTDEPS="${2}"
 # Check if package have dependencies in the
 # 'require' object, inside the composer.json file
 if [[ "${EXTDEPS}" == '1' ]]; then
+	# Fix for Composer 2
+	composer require \
+		--working-dir=phpBB/ext/"${EXTNAME}" \
+		--prefer-dist \
+		--update-with-all-dependencies \
+		'composer/package-versions-deprecated:^1.11.99' \
+		'ocramius/proxy-manager:~2.1.1'
+
 	composer install \
 		--working-dir=phpBB/ext/"${EXTNAME}" \
 		--prefer-dist \
